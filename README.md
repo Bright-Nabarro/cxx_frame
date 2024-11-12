a697f4be6e7a4f71ad8b1cd17f3118142129c7ee
lin17 <lin17>
2024-11-12 21:53:28 +0800

    simpfy llvm target adding

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 1. Clone this frame and rename dir
 ```bash
-git clone https://github.com/Bright-Nabarro/cxx_frame.git
+git clone https://github.com/Bright-Nabarro/cxx_frame.git -b llvm_frame
 mv cxx_frame <project_name>
 ```
 2. Replace xx in project(__xx__ LANGUAGES CXX VERSION 0.0.1) in ./CMakeLists.txt with your project name.
--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -4,3 +4,28 @@ function(ChgExeOutputDir target)
 	)
 endfunction()
 
+macro(AddTrgLibrary name)
+	if (TARGET ${name})
+		message(WARNING "Target ${name} already exists. Skipping AddTrgLibrary")
+		return()
+	endif()
+
+	set (LIBTYPE STATIC)
+	if (BUILD_SHARED_LIBS)
+		set(LIBTYPE SHARED)
+	endif()
+
+	llvm_add_library(${name} ${LIBTYPE} ${ARGN})
+	target_link_libraries(${name} PUBLIC ${LLVM_COMMON_LIBS})
+endmacro()
+
+macro(AddTrgExe name)
+	add_llvm_executable(${name} ${ARGN})
+	#target_include_directories(${name} PUBLIC
+	#	"$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/${trg}>"
+	#	"$<BUILD_INTERFACE:${LLVM_INCLUDE_DIRS}>"
+	#    "$<INSTALL_INTERFACE:include>"
+	#)
+endmacro()
+
+
